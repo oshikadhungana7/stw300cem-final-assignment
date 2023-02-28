9dd68e3c63b55e06ee4e1dbcd1b9cc0f451956ac
oshikadhungana7 <oshikadhungana798@gmail.com>
2023-02-28 22:25:35 +0545

    final

--- FoodGung/test/integration_test/login_integrationTest.dart
+++ FoodGung/test/integration_test/login_integrationTest.dart
@@ -29,3 +29,16 @@ void main() {
   });
   
 }
+Finder txtEmail = find.byKey(const ValueKey('txtEmail'));
+      await tester.enterText(txtEmail, 'khadka03adrsh@gmail.com');
+      Finder txtPassword = find.byKey(const ValueKey('txtPassword'));
+      await tester.enterText(txtPassword, 'Kdk@03adrsh');
+      Finder btnSignIn = find.byKey(const ValueKey('btnSignIn'));
+      await tester.dragUntilVisible(btnSignIn, find.byType(SingleChildScrollView),  const Offset(0, 70),);
+      await tester.tap(btnSignIn);
+      await tester.pumpAndSettle();
+      expect(find.byType(Container),findsOneWidget);
+      // expect(find.text("Login"), findsOneWidget);
+  });
+  
+}
